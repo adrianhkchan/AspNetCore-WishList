254616d17a15b5deebc204eb1c47f227d2b2a9d9
Adrian Chan <adrianhkchan@gmail.com>
2019-01-21 21:09:51 +0000

    Created Item Views

---
+++ WishList/Controllers/ItemController.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace WishList.Controllers
+{
+    public class ItemController : Controller
+    {
+        public IActionResult index()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IActionResult create()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IActionResult delete()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
